fd694f3f772b568f0c5da088356ae367c0f3c9c3
devalexer <alliemckay.dev@gmail.com>
2017-05-03 10:01:43 -0400

    Project created

---
+++ EventSite/Models/EventModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace EventSite.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        public string Description { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public int VenueId { get; set; }
+        public Menu Venue { get; set; }
+
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
---
+++ EventSite/Models/VenueModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventSite.Models
+{
+    public class VenueModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public virtual ICollection<EventModel> Events { get; set; } = new HashSet<EventModel>();
+
+
+    }
+}
